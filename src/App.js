118a9d9a4cc1de55712ead61939b7100da7485ea
Arkistos <pierre.laco@hotmail.fr>
2022-11-16 17:38:53 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import {Link, Outlet, Route, Routes} from 'react-router-dom';
+import Versets from './Versets';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='versets' element={<Versets/>}/>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout(){
+  return (
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/versets'>Versets</Link>
+      </nav>
+      <Outlet/>
+    </div>
+    );
+}
+
+function Home(){
+  return(<h2>Bienvenu</h2>)
+}
+
+
+
+export default App;
+
